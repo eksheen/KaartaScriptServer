d528ef018ec73dfbdf5c328237713f30470a94b3
Evan Sheen <eksheen@gmail.com>
2017-01-02 12:57:14 -0500

    Forms use post instead of get for security

--- kaartaScriptServer.php
+++ kaartaScriptServer.php
@@ -39,7 +39,10 @@ if (!empty($_POST)){
 		&& isset($_POST['matchDis']) && isset($_POST['matchReginHori'])
 		&& isset($_POST['matchReginVert']) && isset($_POST['poseStackNum'])){
 			//Do something here is all values are set
-    	echo "all values set - ";
+    	echo "all values set:<br />\n";
+			foreach ($_POST as $param_name => $param_val) {
+    		echo "$param_name: $param_val<br />\n";
+			}
 		} else {
 			//Handle the case where one or more of the values is not set
 			echo "some value is not set - ";
